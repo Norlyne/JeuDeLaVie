31573223839a052e7fed4f765813e56e48ecc673
Norlyne <160507883+Norlyne@users.noreply.github.com>
2025-11-24 13:54:17 +0100

    Add Jeu class with grid management methods
    Define the Jeu class with a constructor and methods for grid initialization and neighbor counting.

---
+++ POO_Projet_jeu/Jeu.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Grille.h"
+#include "cell.h"
+
+class Jeu{
+private:
+	Grille *grille;
+public:
+	// constructeur
+	Jeu() : public Grille() {
+		grille = new Grille();
+	}
+	// method
+	void initializeGrid();
+	int countvoisin(cell& c);
+
+
+};
+
