f2b3bb69230964f478e566001810caab26518bf2
Caramoel <ggueraud@orange.fr>
2025-11-24 10:51:21 +0100

    Implement constructors and accessors for cell class

--- POO_Projet_jeu/cell.cpp
+++ POO_Projet_jeu/cell.cpp
@@ -1,6 +1,36 @@
 #include "cell.h"
 
-dead_cell::dead_cell() {
+//COnstructors
+cell::cell() {
+	this->x = 0;
+	this->y = 0;
 	this->state = false;
-
 }
+
+cell::cell(int x, int y, bool state) {
+	this->x = x;
+	this->y = y;
+	this->state = state;
+}
+
+dead_cell::dead_cell() {
+	this->x = 0;
+	this->y = 0;
+	this->state = false;
+}
+
+live_cell::live_cell() {
+	this->x = 0;
+	this->y = 0;
+	this->state = false;
+}
+
+//getters
+int cell::getx() { return this->x; }
+int cell::gety() { return this->y; }
+bool cell::get_state() { return this->state;  }
+
+//setters
+void cell::setx(int x) { this->x = x; }
+void cell::sety(int y) { this->y = y; }
+void cell::set_state(bool state) { this->state = state; }
