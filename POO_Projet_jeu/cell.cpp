28fe7593f2a44cfca5a799120bc0c7a80f81f1fd
Caramoel <ggueraud@orange.fr>
2025-11-24 13:35:54 +0100

    Update cell.cpp

--- POO_Projet_jeu/cell.cpp
+++ POO_Projet_jeu/cell.cpp
@@ -1,36 +1,28 @@
 #include "cell.h"
+#include "Grille.h"
 
 //COnstructors
 cell::cell() {
-	this->x = 0;
-	this->y = 0;
 	this->state = false;
 }
 
-cell::cell(int x, int y, bool state) {
-	this->x = x;
-	this->y = y;
+cell::cell(bool state) {
 	this->state = state;
 }
 
-dead_cell::dead_cell() {
-	this->x = 0;
-	this->y = 0;
+dead_cell::dead_cell(sf::RenderWindow& window, int x, int y) {
 	this->state = false;
+	dessiner(window, x, y);
 }
 
-live_cell::live_cell() {
-	this->x = 0;
-	this->y = 0;
-	this->state = false;
+live_cell::live_cell(sf::RenderWindow& window, int x, int y) {
+	this->state = true;
+	dessiner(window, x, y);
 }
 
 //getters
-int cell::getx() { return this->x; }
-int cell::gety() { return this->y; }
-bool cell::get_state() { return this->state;  }
-
+bool cell::get_state() { return this->state; }
+ 
 //setters
-void cell::setx(int x) { this->x = x; }
-void cell::sety(int y) { this->y = y; }
 void cell::set_state(bool state) { this->state = state; }
+
