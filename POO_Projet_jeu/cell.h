9ac70cad1eb56853b3129e13d18d316678682047
Caramoel <ggueraud@orange.fr>
2025-11-24 13:36:33 +0100

    Refactor cell class and update drawing methods

--- POO_Projet_jeu/cell.h
+++ POO_Projet_jeu/cell.h
@@ -2,58 +2,55 @@
 class cell
 {
 protected:
-	int x;
-	int y;
-	bool state;
+	bool state; // peut etre pas besoin puisqu'on ne crée qu'un seul objet
+	int cellSize;
 
 public:
 	//Constructor
 	cell();
-	cell(int x, int y, bool state);
+	cell(bool state);
 
 	//getters
-	int getx();
-	int gety();
 	bool get_state();
 
 	//setters
-	void setx(int x);
-	void sety(int y);
 	void set_state(bool state);
 
 	//methodes
-	virtual void set_color() = 0;
+	virtual void dessiner(sf::RenderWindow& window, int x, int y) = 0;
 };
 
 
 
-class dead_cell : cell {
-private:
-
-
-
+class dead_cell : public cell {
 public:
 	//Constructor
 	dead_cell();
+	dead_cell(sf::RenderWindow& window, int x, int y);
+	
 
 	//methodes
-	void set_color() override {
-
+	void dessiner(sf::RenderWindow& window, int x, int y) override {
+		sf::RectangleShape cell(sf::Vector2f(this->cellSize - 1.0f, this->cellSize - 1.0f));
+		cell.setPosition(x * this->cellSize, y * this->cellSize);
+		cell.setFillColor(sf::Color(0, 0, 0));
+		window.draw(cell);
 	}
 };
 
 
-class live_cell : cell {
-private:
-
-
+class live_cell : public cell {
 public:
 	//Constructor
 	live_cell();
+	live_cell(sf::RenderWindow& window, int x, int y);
 
 	//methodes
-	void set_color() override {
-
+	void dessiner(sf::RenderWindow& window, int x, int y) override {
+		sf::RectangleShape cell(sf::Vector2f(this->cellSize - 1.0f, this->cellSize - 1.0f));
+		cell.setPosition(x * this->cellSize, y * this->cellSize);
+		cell.setFillColor(sf::Color(255, 255, 255));
+		window.draw(cell);
 	}
 };
 
