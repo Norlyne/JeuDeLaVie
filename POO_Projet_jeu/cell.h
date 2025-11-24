82327ceb86e3b3c44ca04e5236bb1f731ef00a86
Caramoel <ggueraud@orange.fr>
2025-11-24 10:26:08 +0100

    Add cell, dead_cell, and live_cell classes

---
+++ POO_Projet_jeu/cell.h
@@ -0,0 +1,71 @@
+#pragma once
+class cell
+{
+protected:
+	int x;
+	int y;
+
+
+public:
+	//Constructor
+	cell();
+
+	//getters
+	int getx();
+	int gety();
+
+	//setters
+	void setx();
+	void sety();
+
+	//methodes
+	virtual void set_color() = 0;
+};
+
+
+
+class dead_cell : cell {
+private:
+	bool state;
+
+
+public:
+	//Constructor
+	dead_cell();
+
+	//getter
+	bool get_state();
+
+	//setters
+	void set_state(bool state);
+
+	//methodes
+	void set_color() override {
+
+	}
+};
+
+
+class live_cell : cell {
+private:
+	bool state;
+
+
+public:
+	//Constructor
+	live_cell();
+
+	//getters
+	bool get_state();
+
+	//setters
+	void set_state(bool state);
+
+	//methodes
+	void color() override {
+
+	}
+};
+
+
+
