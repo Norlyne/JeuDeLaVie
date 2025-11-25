c731c74575ef381a7b223472fe15b9e4ae106a99
Caramoel <ggueraud@orange.fr>
2025-11-25 11:49:27 +0100

    dfh

--- POO_Projet_jeu/main.cpp
+++ POO_Projet_jeu/main.cpp
@@ -7,12 +7,14 @@ int main()
 {
     std::srand(std::time(0));
 
-    grille grille1;
-    grille1.fichier_init();
+    grille current_grid;
+    grille next_grid;
 
-    for (int dx = 0; dx < grille1.get_width(); dx++) {
-        for (int dy = 0; dy < grille1.get_height(); dy++) {
-            cellule* c = grille1.get_grille(dx, dy);
+    current_grid.fichier_init();
+
+    for (int dx = 0; dx < current_grid.get_width(); dx++) {
+        for (int dy = 0; dy < current_grid.get_height(); dy++) {
+            cellule* c = current_grid.get_grille(dx, dy);
             if (c && c->is_alive()) std::cout << "1";
             else std::cout << "0";
         }
