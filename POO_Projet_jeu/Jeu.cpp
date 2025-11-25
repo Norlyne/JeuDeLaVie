e8f54afdbc9e5ac60c03eaaa48c400edd06bd9e5
Caramoel <ggueraud@orange.fr>
2025-11-25 14:10:49 +0100

    serh

--- POO_Projet_jeu/Jeu.cpp
+++ POO_Projet_jeu/Jeu.cpp
@@ -1,24 +1,4 @@
 #include "jeu.h"
-//
-//void jeu::regle_base(grille current_grid, grille new_grid) {
-//    int nb_voisin;
-//    for (int dx = 0; dx < current_grid.get_width(); dx++) {
-//        for (int dy = 0; dy < current_grid.get_height(); dy++) {
-//            nb_voisin = compt_voisin();
-//            cellule* current = grille1.get_grille(dx, dy)
-//         
-//                if (grid[dx][dy]) delete grid[dx][dy];  // a revoir
-//                grid[dx][dy] = new cellule_vivante();
-//         
-//        
-//                if (grid[dx][dy]) delete grid[dx][dy];
-//                grid[dx][dy] = new cellule_morte();
-//           
-//        }
-//    }
-//}
-// 
-//
 
 void jeu::regle_base(grille &grid, grille &next) {
     for (int x = 0; x < grid.get_width(); ++x) {
