146fff565e878b7bf2486e01c1e6434826d0c0a4
Norlyne <norlyne648@gmail.com>
2025-11-27 11:05:18 +0100

    zertyui

--- POO_Projet_jeu/Jeu.h
+++ POO_Projet_jeu/Jeu.h
@@ -231,4 +231,144 @@ public:
 
 #pragma endregion
 
+     #pragma region labyrinthiques
+
+class labyrinthiques : public jeu {
+public:
+    void regle_base(grille& grid, grille& next) override {
+        for (int x = 0; x < grid.get_width(); ++x) {
+            for (int y = 0; y < grid.get_height(); ++y) {
+                int n = grid.compt_voisin_thorique(x, y);
+                if (grid.get_grille(x, y)->is_alive() == 1) {
+                    // cellule vivante
+                    if (n == 2 || n == 3 || n == 4 || n == 5) {
+                        next.set_grille(x, y, 1); // survit
+                    }
+                    else {
+                        next.set_grille(x, y, 0); // meurt
+                    }
+                }
+                else if (grid.get_grille(x, y)->is_alive() == 0) {
+                    // cellule morte
+                    if (n == 3) {
+                        next.set_grille(x, y, 1); // naissance
+                    }
+                    else {
+                        next.set_grille(x, y, 0);
+                    }
+                }
+                else {
+                    next.set_grille(x, y, 2);
+                }
+            }
+        }
+        /// swap 
+        for (int dx = 0; dx < next.get_width(); dx++) {
+            for (int dy = 0; dy < next.get_height(); dy++) {
+                if (next.get_grille(dx, dy)->is_alive() == 1) {
+                    grid.set_grille(dx, dy, 1);
+                }
+                else if (next.get_grille(dx, dy)->is_alive() == 0) {
+                    grid.set_grille(dx, dy, 0);
+                }
+                else {
+                    grid.set_grille(dx, dy, 2);
+                }
+            }
+        }
+    }
+
+    void dessin_rectangle(RenderWindow& window, grille grid) override {
+        int x, y;
+
+        window.clear();
+        RectangleShape cell(Vector2f(10 - 1.0f, 10 - 1.0f));
+        for (x = 0; x < grid.get_width(); ++x) {
+            for (y = 0; y < grid.get_height(); ++y) {
+                if (grid.get_grille(x, y)->is_alive() == 1) {
+                    cell.setPosition(x * 10, y * 10);
+                    cell.setFillColor(Color(rand() % 1 + x, 255, rand() % 1 + x));
+                    window.draw(cell);
+                }
+                else if (grid.get_grille(x, y)->is_alive() == 2) {
+                    cell.setPosition(x * 10, y * 10);
+                    cell.setFillColor(sf::Color(128, 128, 128));
+                    window.draw(cell);
+                }
+            }
+        }
+        window.display();
+    }
+};
+
+#pragma endregion
+
+     #pragma region ExplosionsAndChaos
+
+class ExplosionsAndChaos : public jeu {
+public:
+    void regle_base(grille& grid, grille& next) override {
+        for (int x = 0; x < grid.get_width(); ++x) {
+            for (int y = 0; y < grid.get_height(); ++y) {
+                int n = grid.compt_voisin_thorique(x, y);
+                if (grid.get_grille(x, y)->is_alive() == 1) {
+                    // cellule vivante
+                    next.set_grille(x, y, 0); // meurt
+                }
+                else if (grid.get_grille(x, y)->is_alive() == 0) {
+                    // cellule morte
+                    if (n == 2) {
+                        next.set_grille(x, y, 1); // naissance
+                    }
+                    else {
+                        next.set_grille(x, y, 0);
+                    }
+                }
+                else {
+                    next.set_grille(x, y, 2);
+                }
+            }
+        }
+        /// swap 
+        for (int dx = 0; dx < next.get_width(); dx++) {
+            for (int dy = 0; dy < next.get_height(); dy++) {
+                if (next.get_grille(dx, dy)->is_alive() == 1) {
+                    grid.set_grille(dx, dy, 1);
+                }
+                else if (next.get_grille(dx, dy)->is_alive() == 0) {
+                    grid.set_grille(dx, dy, 0);
+                }
+                else {
+                    grid.set_grille(dx, dy, 2);
+                }
+            }
+        }
+    }
+
+    void dessin_rectangle(RenderWindow& window, grille grid) override {
+        int x, y;
+
+        window.clear();
+        RectangleShape cell(Vector2f(10 - 1.0f, 10 - 1.0f));
+        for (x = 0; x < grid.get_width(); ++x) {
+            for (y = 0; y < grid.get_height(); ++y) {
+                if (grid.get_grille(x, y)->is_alive() == 1) {
+                    cell.setPosition(x * 10, y * 10);
+                    cell.setFillColor(Color(rand() % 1 + x, rand() % 255, rand() % 1 + x));
+                    window.draw(cell);
+                }
+                else if (grid.get_grille(x, y)->is_alive() == 2) {
+                    cell.setPosition(x * 10, y * 10);
+                    cell.setFillColor(sf::Color(128, 128, 128));
+                    window.draw(cell);
+                }
+            }
+        }
+        window.display();
+    }
+};
+
+#pragma endregion
+
+
 #pragma endregion
